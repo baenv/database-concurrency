6fc273e1fad614c900406bbb194e47acbef1576a
Tom X Nguyen <tom81094@gmail.com>
2023-01-05 10:50:56 +0700

    test: add shortest paths test

--- internal/transducer/booking_test.go
+++ internal/transducer/booking_test.go
@@ -1,6 +1,8 @@
 package transducer
 
 import (
+	"database-concurrency/internal/transducer/graph"
+	"fmt"
 	"testing"
 )
 
@@ -14,4 +16,30 @@ func TestRehearseBookingTransducer(t *testing.T) {
 
 	digraph := bookingTransducer.ToDiGraph()
 	t.Logf("Digraph: %v\n", digraph)
+}
+
+func TestBookingShortestPaths(t *testing.T) {
+	characterConfig := CreateConfig().SetState(Idle)
+	characterTransducer := NewBookingTransducer(characterConfig)
+
+	paths, edges := characterTransducer.GetShortestPaths()
+	currentState := graph.Vertex(Idle)
+
+	for _, path := range paths {
+		for j, state := range path {
+			if path[0] != currentState {
+				stateLog := fmt.Sprintf("\n'%v' state", path[0])
+				currentState = path[0]
+				fmt.Println(stateLog)
+			}
+
+			if j < len(path)-1 {
+				nextState := path[j+1]
+
+				input := edges[state][nextState]
+				transitionLog := fmt.Sprintf("\t\t%-30v + %-30v -> %-30v", state, input, nextState)
+				fmt.Println(transitionLog)
+			}
+		}
+	}
 }

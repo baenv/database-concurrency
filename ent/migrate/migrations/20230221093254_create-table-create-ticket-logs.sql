ea6d44125a99f1dba48cc684f9a1468c91b4da91
swomfire <vucuongJim@gmail.com>
2023-02-28 10:41:45 +0700

    Update migrate

--- ent/migrate/migrations/20230221093254_create-table-create-ticket-logs.sql
+++
@@ -1,4 +0,0 @@
--- create "create_ticket_logs" table
-CREATE TABLE "create_ticket_logs" ("ticket_id" uuid NOT NULL, "unique_id" uuid NOT NULL, "created_at" timestamptz NOT NULL, "updated_at" timestamptz NOT NULL, PRIMARY KEY ("ticket_id"));
--- create index "create_ticket_logs_unique_id_key" to table: "create_ticket_logs"
-CREATE UNIQUE INDEX "create_ticket_logs_unique_id_key" ON "create_ticket_logs" ("unique_id");
